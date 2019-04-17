04c256b8147391118145f4006c34708bc362fa1f
Deberson Paula <deberson.paula@xpi.com.br>
2019-04-17 18:13:17 -0300

    added Dependency

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,8 @@
 import { Action } from './ReduxAction';
 import { Connection } from './ReduxConnection';
+import { Dependency } from './ReduxDependency';
 import { Inject } from './ReduxInject';
 import { Model } from './ReduxModel';
 import { ModelViewer } from './ReduxModelViewer';
 
-export { Action, Connection, Inject, Model, ModelViewer };
+export { Action, Connection, Dependency, Inject, Model, ModelViewer };
