e2a6770815c3b865d0a75bd8572f28f9e2ab0e35
Deberson Paula <deberson.paula@gmail.com>
2019-06-01 11:22:21 -0300

    fixed config

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -1,61 +1,49 @@
-const path = require('path');
-const webpack = require('webpack');
-const rootDir = path.resolve(__dirname, '../');
-const TsConfigPathsPlugin = require('awesome-typescript-loader')
+const path = require("path");
+const webpack = require("webpack");
+const rootDir = path.resolve(__dirname, "../");
+const TsConfigPathsPlugin = require("awesome-typescript-loader")
   .TsConfigPathsPlugin;
-const HTMLWebpackPlugin = require('html-webpack-plugin');
+const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   devServer: {
-    port: '8001',
+    port: "8001",
     hot: true,
     open: true,
     historyApiFallback: true
   },
-  entry: ['react-hot-loader/patch', path.join(rootDir, 'playground/index.tsx')],
+  entry: [path.join(rootDir, "playground/index.tsx")],
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        // loader: 'babel-loader'
-        loader: 'ts-loader'
+        loader: "babel-loader"
       },
-      // {
-      //   enforce: 'pre',
-      //   test: /\.js$/,
-      //   loader: 'source-map-loader',
-      //   exclude: [path.join(process.cwd(), 'node_modules')]
-      // },
-      // {
-      //   test: /\.scss$/,
-      //   loader: 'style-loader!css-loader!sass-loader'
-      // },
-      // {
-      //   test: /\.(jpe?g|png|gif|svg)$/i,
-      //   loader: 'url-loader',
-      //   options: {
-      //     limit: 10000
-      //   }
-      // }
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader",
+        exclude: [path.join(process.cwd(), "node_modules")]
+      }
     ]
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
+    extensions: [".ts", ".tsx", ".js"],
     plugins: [new TsConfigPathsPlugin()]
   },
   output: {
-    filename: 'index.js',
-    path: path.join(rootDir, 'dist')
+    filename: "index.js",
+    path: path.join(rootDir, "dist")
   },
   plugins: [
     new HTMLWebpackPlugin({
-      template: path.join(rootDir, 'playground/index.html'),
+      template: path.join(rootDir, "playground/index.html"),
       inject: true
     }),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.DefinePlugin({
-      'process.env': {
+      "process.env": {
         NODE_ENV: JSON.stringify(process.env.NODE_ENV)
       }
     })
