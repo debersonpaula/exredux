8b25ba6a73473022c27cb082f33210c5973ebdcb
Deberson Paula <deberson.paula@gmail.com>
2019-06-04 08:57:11 -0300

    chore: updated build cfg

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018
+    },
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
--- deploy/postbuild.js
+++ deploy/postbuild.js
@@ -1,12 +1,7 @@
-const fs = require('fs');
-// const def = "export * from './deploy';";
-// fs.writeFileSync('./dist/index.d.ts', def);
+const fs = require("fs");
 
-const packageFile = fs.readFileSync('./package.json');
+const packageFile = fs.readFileSync("./package.json");
 const packageObject = JSON.parse(packageFile);
-packageObject.devDependencies = {};
 packageObject.scripts = {};
-fs.writeFileSync('./dist/package.json', JSON.stringify(packageObject));
-// fs.writeFileSync('./dist/index.d.ts', "export * from './src';");
-
+fs.writeFileSync("./dist/package.json", JSON.stringify(packageObject));
 fs.copyFileSync("./README.md", "./dist/README.md");
