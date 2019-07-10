035367ef19e4db2fc01d3cd529dcc8cc4133fb16
Deberson Paula <deberson.paula@gmail.com>
2019-07-10 14:09:54 -0300

    fixed modelStore ref

--- src/libExRedux/Provider.tsx
+++ src/libExRedux/Provider.tsx
@@ -4,29 +4,27 @@ import { Store } from 'redux';
 import { ModelStore } from './ModelStore';
 
 interface Props {
-  store?: Store;
   modelStore?: ModelStore;
-  suppressWarning?: boolean;
 }
 
 export class Provider extends React.Component<Props> {
+  store: Store;
+
+  constructor(props: Props) {
+    super(props);
+    if (props.modelStore) {
+      this.store = props.modelStore.createStore();
+    } else {
+      // tslint:disable-next-line: no-console
+      console.warn('No store provided! The children will be rendered directly without Redux Provider');
+    }
+  }
+
   public render() {
-    const { store, modelStore, children, suppressWarning } = this.props;
-    if (modelStore) {
-      const createdStore = modelStore.createStore();
-      return <ReactProvider store={createdStore}>{children}</ReactProvider>;
+    const { children } = this.props;
+    if (this.store) {
+      return <ReactProvider store={this.store}>{children}</ReactProvider>;
     }
-    if (store) {
-      // tslint:disable-next-line: no-console
-      console.warn('DEPRECATION WARNING: USAGE of <Provider store={modelStore.createStore()}> is DEPRECATED in the Provider and will be removed');
-      // tslint:disable-next-line: no-console
-      console.warn('PLEASE USE <Provider modelStore={modelStore}> INSTEAD!');
-
-      return <ReactProvider store={store}>{children}</ReactProvider>;
-    }
-
-    // tslint:disable-next-line: no-console
-    !suppressWarning && console.warn('No store provided! The children will be rendered directly without Redux Provider');
     return <React.Fragment>{children}</React.Fragment>;
   }
 }
